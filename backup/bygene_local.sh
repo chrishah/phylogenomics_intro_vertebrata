fb4b460e5d020db9c4b758db3630e954d56ee7b4
chrishah <chrisi.hahni@gmail.com>
2023-04-05 00:46:58 +0200

    generalize local for loop

--- backup/bygene_local.sh
+++ backup/bygene_local.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+sif_file_path="~/Share/Singularity_images"
+
 for gene in $(echo "359032at7742 413149at7742 409719at7742 406935at7742")
 do
         echo -e "\n$(date)\t$gene"
         echo -e "$(date)\taligning"
-        singularity exec ~/Share/Singularity_images/clustalo-docker_1.2.4.sif clustalo -i by_gene/raw/${gene}_all.fas -o by_gene/aligned/${gene}.clustalo.fasta --threads=2
+        singularity exec $sif_file_path/clustalo-docker_1.2.4.sif clustalo -i by_gene/raw/${gene}_all.fas -o by_gene/aligned/${gene}.clustalo.fasta --threads=2
         echo -e "$(date)\ttrimming"
-        singularity exec ~/Share/Singularity_images/trimal_1.4.1.sif trimal -in by_gene/aligned/${gene}.clustalo.fasta -out by_gene/trimmed/${gene}.clustalo.trimal.fasta -gappyout
+        singularity exec $sif_file_path/trimal_1.4.1.sif trimal -in by_gene/aligned/${gene}.clustalo.fasta -out by_gene/trimmed/${gene}.clustalo.trimal.fasta -gappyout
         mkdir -p by_gene/phylogeny/${gene}
-        singularity exec ~/Share/Singularity_images/iqtree_2.0.7.sif \
+        singularity exec $sif_file_path/iqtree_2.0.7.sif \
               iqtree \
               -s by_gene/trimmed/${gene}.clustalo.trimal.fasta \
               --prefix by_gene/phylogeny/${gene}/${gene} \
